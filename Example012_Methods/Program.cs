993912a462add9b5b2927b857f0f221d38838158
Evgeniia_Makarova <evgetigra@gmail.com>
2022-09-15 11:51:47 +0300

    корректировки в папке Example012_Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,29 +1,29 @@
 ﻿//Вид 1 - ничего не возвращает, ничего не принимает
 
-void Method1()
-{
-    Console.WriteLine("Автор ...");
-}
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
 
 //Method1();
 
 //Вид 2 - ничего не возвращают, но могут принимать какой-то аргумент
 
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-//Method2(msg: "Текст сообщения");
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// //Method2(msg: "Текст сообщения");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
 
 // Method21(msg: "Текст", count: 4);
 //Method21(count: 4, msg: "Новый Текст");
